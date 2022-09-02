b32978633d50b4a440d5c194f1dfb2389ee95981
George <gnjuki19@gmail.com>
2022-09-02 22:55:37 +0300

    Created the data for the tables in our database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,26 @@
 puts "I am a seeding file"
 
-20.times do 
-    Student.create(
-        name: Faker::Name.name,
-        cohort_id: cohort.id,
-        lecturer_id: lecturer.id
-        )
-end
+# Creates the students data
+Student.create(name: "George Njuki", course: "Software Development", cohort_id: cohort.id, lecturer_id: lecturer.id)
+Student.create(name: "Mariam Ali", course: "Cybersecurity", cohort_id: cohort.id, lecturer_id: lecturer.id )
+Student.create(name: "Ian Ngila", course: "Software Development", cohort_id: cohort.id, lecturer_id: lecturer.id )
+Student.create(name: "Irene Njuguna", course: "Data Science", cohort_id: cohort.id, lecturer_id: lecturer.id )
+Student.create(name: "Allan Mauko", course: "Software Development", cohort_id: cohort.id, lecturer_id: lecturer.id )
+Student.create(name: "Sauroki Festus", course: "Data Science", cohort_id: cohort.id, lecturer_id: lecturer.id )
+Student.create(name: "Anthony Kimeu", course: "Software Development", cohort_id: cohort.id, lecturer_id: lecturer.id )
+Student.create(name: "Winston Kiprop", course: "Cybersecurity", cohort_id: cohort.id, lecturer_id: lecturer.id )
 
-20.time do
-    Lecturer.create(
-        name: Faker::Name.name,
-        cohort_id: cohort.id
-    )
-end
 
-10.time do
-    Cohort.create(
-        name: rand(50..80)
-        student_id: student.id
-    )
-end
+# Create the lecturers data
+Lecturer.create(name: "Howard Mnengwa", course: "Data Science")
+Lecturer.create(name: "Stella Waithera", course: "Cybersecurity")
+Lecturer.create(name: "Mercy Nzau", course: "Data Science")
+Lecturer.create(name: "Kelvin Kipchumba", course: "Software Development")
+
+
+# Creates the courses table
+Courses.create(name: "Sotware Development", lecturer_id: lecturer.id, student_id: student.id)
+Courses.create(name: "Data Science", lecturer_id: lecturer.id, student_id: student.id)
+Courses.create(name: "Cybersecurity", lecturer_id: lecturer.id, student_id: student.id)
 
 puts "Done seeding"
