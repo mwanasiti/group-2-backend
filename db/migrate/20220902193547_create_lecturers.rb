4c288bf25379aca80c3fcfdeed5c851bcc86137d
allanmauko <allan.mauko@student.moringaschool.com>
2022-09-04 17:15:51 +0300

    Changes in production

--- db/migrate/20220902193547_create_lecturers.rb
+++ db/migrate/20220902193547_create_lecturers.rb
@@ -1,4 +1,4 @@
-class CreateLecturers < ActiveRecord::Migration[7.0]
+class CreateLecturers < ActiveRecord::Migration[6.1]
   def change
     create_table :lecturers do |t|
       t.string :name
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_03_123616) do
+ActiveRecord::Schema.define(version: 2022_09_03_123616) do
+
   create_table "courses", force: :cascade do |t|
     t.string "name"
     t.integer "lecturer_id"
